26da91580210b736bb8b1e1a2f703f3df6585410
mdcoquia50405-cmd <mdcoquia50405@liceo.edu.ph>
2025-08-01 09:37:25 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-System.out.println("hello world");
-}
+        Scanner input = new Scanner(System.in);
+
+        int totalNumber = 0;
+
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter number " + i + ": ");
+            int number = input.nextInt();
+            totalNumber += number;
+        }
+
+        System.out.println("\nTotal Number: " + totalNumber);
+
+        input.close();
+    }
 }
