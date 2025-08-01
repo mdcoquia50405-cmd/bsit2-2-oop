e9583aeb77753a1ecb841918c6ad2595c15b2991
Mark Daniel Coquia <mdcoquia50405@liceo.edu.ph>
2025-08-01 09:49:09 +0800

    lint fix

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         int totalNumber = 0;
-
 
         for (int i = 1; i <= 5; i++) {
             System.out.print("Enter number " + i + ": ");
