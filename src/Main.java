84cd97f2520d679e1ecbc30b2972179b51e17c52
Mark Daniel Coquia <mdcoquia50405@liceo.edu.ph>
2025-08-18 11:27:00 +0800

    activity

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-System.out.println("hello world");
+        GradeCalculator calculator = new GradeCalculator();
+
+
+        String studentName = "John Smith";
+        double average = calculator.calculateAverage(85.5, 92.0, 78.5, 90.0);
+        String letterGrade = calculator.getLetterGrade(average);
+
+        System.out.println("first displayResult");
+        calculator.displayResult(studentName, average);
+
+        System.out.println("\nUsing second displayResult");
+        calculator.displayResult(studentName, average, letterGrade);
+    }
 }
-}
+
+
