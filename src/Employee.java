b590228259d381c6a832033f9d5328c57c7d5c8c
mdcoquia50405-cmd <mdcoquia50405@liceo.edu.ph>
2025-09-11 11:20:48 +0800

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,28 +1,29 @@
-public class Intern extends Employee {
-    private String university;
-    private boolean isFullTime;
+public class Employee {
+    protected String name;
+    protected int employeeId;
+    protected double baseSalary;
+    protected String department;
 
-    public Intern(String name, int employeeId, double baseSalary, String department, String university, boolean isFullTime) {
-        super(name, employeeId, baseSalary, department);
-        this.university = university;
-        this.isFullTime = isFullTime;
-        System.out.println("Intern " + name + " from " + university + " has started");
+    public Employee(String name, int employeeId, double baseSalary, String department) {
+        this.name = name;
+        this.employeeId = employeeId;
+        this.baseSalary = baseSalary;
+        this.department = department;
+        System.out.println("Employee " + name + " has been hired in " + department + " department");
     }
 
-    @Override
-    public double calculateSalary() {
-        return baseSalary * 0.5; // Interns get 50% of base salary
-    }
-
-    @Override
-    public void work() {
-        System.out.println(name + " is learning and assisting with tasks");
-    }
-
-    @Override
     public void displayInfo() {
-        super.displayInfo();
-        System.out.println("University: " + university);
-        System.out.println("Full-Time: " + (isFullTime ? "Yes" : "No"));
+        System.out.println("Name: " + name);
+        System.out.println("Employee ID: " + employeeId);
+        System.out.println("Base Salary: $" + baseSalary);
+        System.out.println("Department: " + department);
+    }
+
+    public double calculateSalary() {
+        return baseSalary;
+    }
+
+    public void work() {
+        System.out.println(name + " is working on general tasks");
     }
 }
